c1873117d59b4481eb9255c3d4bfa1b47709d197
Sihun Kim <skim585@asu.edu>
2022-12-15 01:06:03 -0700

    feat(*): add authentication

--- functions/add-movie.js
+++ functions/add-movie.js
@@ -1,19 +1,29 @@
 const { query } = require('./util/hasura')
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   const { id, title, tagline, poster } = JSON.parse(event.body)
+  const { user } = context.clientContext
+  const isLoggedIn = user && user.app_metadata
+  const roles = user.app_metadata.roles || []
+
+  if (!isLoggedIn || !roles.includes('admin')) {
+    return {
+      statusCode: 401,
+      body: 'Unauthorized',
+    }
+  }
 
   const result = await query({
     query: `
-    mutation AddMovie($id: String!, $poster: String!, $tagline: String!, $title: String!) {
-        insert_movies_one(object: {id: $id, poster: $poster, tagline: $tagline, title: $title}) {
-          id
-          poster
-          tagline
-          title
+      mutation AddMovie($id: String!, $poster: String!, $tagline: String!, $title: String!) {
+          insert_movies_one(object: {id: $id, poster: $poster, tagline: $tagline, title: $title}) {
+            id
+            poster
+            tagline
+            title
+          }
         }
-      }
-    `,
+      `,
     variables: { id, title, tagline, poster },
   })
 
